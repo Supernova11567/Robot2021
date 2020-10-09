2b3facec74cd3cd235c612816a082f64710fa8ba
Idan <idanro12@gmail.com>
2020-10-09 11:13:46 +0300

    Update Supernova11567Main.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Supernova11567Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Supernova11567Main.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="Supernova11567Main", group="supernova11567")
+@TeleOp(name = "Supernova11567Main", group = "supernova11567")
 
 public class Supernova11567Main extends OpMode {
     private ElapsedTime runtime = new ElapsedTime();
@@ -15,22 +15,6 @@ public class Supernova11567Main extends OpMode {
     private DcMotor w1 = null;
     private DcMotor w2 = null;
     private DcMotor w3 = null;
-
-    private double w0StartPosition = 0;
-    private double w1StartPosition = 0;
-    private double w2StartPosition = 0;
-    private double w3StartPosition = 0;
-
-    private boolean rightSync = false;
-    private boolean leftSync = false;
-    private double w0Sync = 0;
-    private double w1Sync = 0;
-    private double w2Sync = 0;
-    private double w3Sync = 0;
-
-    private double speed = 1;  //the value is template...
-    private double minSpeed = 0.2;  //the value is template...
-    private double maxSpeed = 1;  //the value is template...
 
     private double rightBrake = 0;
     private double leftBrake = 0;
@@ -42,14 +26,13 @@ public class Supernova11567Main extends OpMode {
 
 
     //for display
-    private double speedPercentages = (speed / 1) * 100;  //the percentages depend the maximum speed(1)
     private double rightBrakePercentages = (gamepad1.right_trigger / 1) * 100;  //the percentages depend the maximum trigger value(1)
     private double leftBrakePercentages = (gamepad1.left_trigger / 1) * 100;  //the percentages depends the maximum trigger value(1)
     private double MPS = 0;  //the average speed of the motors (meters per second)
 
 
     @Override
-        public void init() {
+    public void init() {
         DcMotor w0 = hardwareMap.get(DcMotor.class, "motor_0");
         DcMotor w1 = hardwareMap.get(DcMotor.class, "motor_1");
         DcMotor w2 = hardwareMap.get(DcMotor.class, "motor_2");
@@ -84,69 +67,24 @@ public class Supernova11567Main extends OpMode {
     @Override
     public void loop() {
 
-        //speed
-        if (gamepad1.right_bumper){
-            speed += 0.4;
-            Range.clip(speed, minSpeed, maxSpeed);
-        }
-        if (gamepad1.left_bumper){
-            speed -= 0.4;
-            Range.clip(speed, minSpeed, maxSpeed);
-        }
-
         //brake
         rightBrake = -gamepad1.right_trigger + 1;
         leftBrake = -gamepad1.left_trigger + 1;
 
-        //sync
-        if (gamepad1.dpad_right){
-            leftSync = false;
-            rightSync = !rightSync;
-            if (rightSync) {
-                w0Sync = -1;
-                w1Sync = -1;
-                w2Sync = -1;
-                w3Sync = -1;
-            }
-            else {
-                w0Sync = 0;
-                w1Sync = 0;
-                w2Sync = 0;
-                w3Sync = 0;
-            }
-        }
-        if (gamepad1.dpad_left) {
-            rightSync = false;
-            leftSync = !leftSync;
-            if (leftSync) {
-                w0Sync = 1;
-                w1Sync = 1;
-                w2Sync = 1;
-                w3Sync = 1;
-            }
-            else {
-                w0Sync = 0;
-                w1Sync = 0;
-                w2Sync = 0;
-                w3Sync = 0;
-            }
-        }
 
         //final move
-        w0.setPower( (-gamepad1.right_stick_y - gamepad1.right_stick_x) * rightBrake * speed + w0Sync);
-        w1.setPower( (gamepad1.left_stick_y   - gamepad1.left_stick_x)  * leftBrake  * speed + w1Sync);
-        w2.setPower( (-gamepad1.right_stick_y + gamepad1.right_stick_x) * rightBrake * speed + w2Sync);
-        w3.setPower( (gamepad1.left_stick_y   + gamepad1.left_stick_x)  * leftBrake  * speed + w3Sync);
+        w0.setPower((-gamepad1.right_stick_y - gamepad1.right_stick_x) * rightBrake);
+        w1.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x) * leftBrake);
+        w2.setPower((-gamepad1.right_stick_y + gamepad1.right_stick_x) * rightBrake);
+        w3.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x) * leftBrake);
 
 
         //display for drivers
         MPS = MPS();
-        speedPercentages = (speed / 1) * 100;
         rightBrakePercentages = (gamepad1.right_trigger / 1) * 100;
         leftBrakePercentages = (gamepad1.left_trigger / 1) * 100;
 
         telemetry.addData("meters per second: ", MPS);
-        telemetry.addData("speed: ", speedPercentages + "%");
         telemetry.addData("rightBrake: ", rightBrakePercentages + "%");
         telemetry.addData("leftBrake: ", leftBrakePercentages + "%");
         telemetry.update();
@@ -156,6 +94,7 @@ public class Supernova11567Main extends OpMode {
     @Override
     public void stop() {
     }
+
 
     public double MPS() {
         double w0PositionBeforeCalculate = w0.getCurrentPosition();
@@ -181,33 +120,31 @@ public class Supernova11567Main extends OpMode {
     }
 
 
-    public double getJoystickXValue (double angle) {
+    public double getJoystickXValue(double angle) {
         double newAngleX = angle;
         double xValue;
 
         if (Math.abs(newAngleX) > 179) {
             newAngleX = 0;
-        }
-        else {
+        } else {
             newAngleX = Range.clip(newAngleX, -179, 179);
         }
 
         if (Math.abs(newAngleX) > 90) {
-            xValue = ( (Math.abs(newAngleX) / 90) - 2* ( (Math.abs(newAngleX) / 90) % 1 ) ) * ( newAngleX / Math.abs(newAngleX) );
+            xValue = ((Math.abs(newAngleX) / 90) - 2 * ((Math.abs(newAngleX) / 90) % 1)) * (newAngleX / Math.abs(newAngleX));
             return xValue;
-        }
-        else {
+        } else {
             xValue = newAngleX / 90;
             return xValue;
         }
     }
 
 
-    public double getJoystickYValue (double angle) {
+    public double getJoystickYValue(double angle) {
         double newAngleY = angle;
         double yValue;
 
-        yValue = ( Math.abs(newAngleY) / 90 ) - 1;
+        yValue = (Math.abs(newAngleY) / 90) - 1;
 
         return yValue;
     }
