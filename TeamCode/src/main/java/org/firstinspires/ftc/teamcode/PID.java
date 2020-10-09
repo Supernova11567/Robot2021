be8472fb8481a7547f0725dabd401f9d0d25df66
Eitan Vichik <godboi524@gmail.com>
2020-10-09 14:35:01 +0300

    Create PID class
    Should be used in multiple systems

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+
+public class PID {
+
+    private PIDCoefficients pid;
+    private double target;
+    private double lastError = 0;
+    private double integral = 0;
+
+    public PID(double k_p, double k_i, double k_d) {
+        pid = new PIDCoefficients(k_p, k_i, k_d);
+    }
+
+    //sets the target of the pid
+    public void setTarget(double in) {
+        this.target = in;
+    }
+
+    public double OutputForce(double input, double deltaTime) {
+        double error = target - input;
+
+        double p = pid.p * error;
+
+        integral += error * deltaTime;
+        double i = pid.i * integral;
+
+        double delta_e = error - lastError;
+        double derivative = delta_e/deltaTime;
+        double d = pid.d * derivative;
+
+        double force = p + i + d;
+
+        lastError = error;
+        return force;
+    }
+}
