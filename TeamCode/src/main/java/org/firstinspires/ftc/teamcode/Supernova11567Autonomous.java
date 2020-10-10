fa050b9eeffb04687f89518a63fe8412825f1efb
Idan <idanro12@gmail.com>
2020-10-10 17:13:50 +0300

    add supernova11567autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Supernova11567Autonomous.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="Pushbot: Auto Drive By Encoder", group="Pushbot")
+
+public class Supernova11567Autonomous extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+
+        waitForStart();
+
+    }
+}
