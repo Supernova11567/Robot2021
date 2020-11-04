aea66f225d4f18ba3758da33afbd264c0c2e3544
Idan <idanro12@gmail.com>
2020-11-04 13:42:32 +0200

    Create IntakeSystem.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeSystem.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class IntakeSystem {
+
+    /* local OpMode members. */
+    private ElapsedTime runtime = new ElapsedTime();
+    private RobotMotorsSetup RobotMotorsSetup = null;
+    private Gamepad gamepad1 = null;
+    private Gamepad gamepad2 = null;
+
+
+    /* Constructor */
+    public IntakeSystem(RobotMotorsSetup RobotMotorsSetup, Gamepad gamepad1, Gamepad gamepad2){
+        this.gamepad1 = gamepad1;
+        this.gamepad2 = gamepad2;
+        this.RobotMotorsSetup = RobotMotorsSetup;
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init() {
+
+    }
+
+    public void loop() {
+
+        
+    }
+
+}
+
