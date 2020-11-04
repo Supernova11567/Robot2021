5ee04ca3007c2e40d0336a2f8b63e63ca8d50a79
Idan <idanro12@gmail.com>
2020-11-04 12:04:25 +0200

    move supernova main loop into RobotmotorsSetup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WheelsSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WheelsSystem.java
@@ -5,21 +5,28 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class WheelsSystem
-{
+public class WheelsSystem {
 
     /* local OpMode members. */
-    private HardwareMap HardwareMap = null;
+    private ElapsedTime runtime = new ElapsedTime();
+    private RobotMotorsSetup RobotMotorsSetup = null;
     private Gamepad gamepad1 = null;
     private Gamepad gamepad2 = null;
-    private ElapsedTime period = new ElapsedTime();
+
+    private double rightBrake = 1;
+    private double leftBrake = 1;
+
+    private double wheelDiameter = 10;  //the value is template...
+
+    private double ticksPerRotation = 1000;  //the value is template...
+    private double ticksPerInch = ticksPerRotation / (wheelDiameter * Math.PI);
+
 
     /* Constructor */
-    public WheelsSystem(HardwareMap hwMap, Gamepad gamepad1, Gamepad gamepad2){
-        this.HardwareMap = hwMap;
+    public WheelsSystem(RobotMotorsSetup RobotMotorsSetup, Gamepad gamepad1, Gamepad gamepad2){
         this.gamepad1 = gamepad1;
         this.gamepad2 = gamepad2;
-
+        this.RobotMotorsSetup = RobotMotorsSetup;
     }
 
     /* Initialize standard Hardware interfaces */
@@ -28,7 +35,11 @@ public class WheelsSystem
     }
 
     public void loop() {
+        //brake
+        rightBrake = -gamepad1.right_trigger + 1;
+        leftBrake = -gamepad1.left_trigger + 1;
 
+        RobotMotorsSetup.moveWheelsByMecanum(rightBrake, leftBrake);
     }
 
 }
