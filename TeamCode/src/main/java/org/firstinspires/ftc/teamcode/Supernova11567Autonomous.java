ad68c28aacb0aac125c77e2acd6fb74dd5c5b52a
Idan <idanro12@gmail.com>
2020-11-04 08:35:04 +0200

    add RobotMotorsSetup to autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Supernova11567Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Supernova11567Autonomous.java
@@ -12,10 +12,7 @@ public class Supernova11567Autonomous extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
 
-    private DcMotor w0 = hardwareMap.get(DcMotor.class, "motor_0");
-    private DcMotor w1 = hardwareMap.get(DcMotor.class, "motor_1");
-    private DcMotor w2 = hardwareMap.get(DcMotor.class, "motor_2");
-    private DcMotor w3 = hardwareMap.get(DcMotor.class, "motor_3");
+    private RobotMotorsSetup RobotMotorsSetup = new RobotMotorsSetup();
     /* motors configuration on robot:
                                            /|\ forward
                                          1     0
@@ -27,21 +24,7 @@ public class Supernova11567Autonomous extends LinearOpMode {
 
         //Until PLAY (after init)
         runtime.reset();
-
-        w0.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        w1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        w2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        w3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        w0.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        w1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        w2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        w3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        w0.setDirection(DcMotorSimple.Direction.FORWARD);
-        w1.setDirection(DcMotorSimple.Direction.FORWARD);
-        w2.setDirection(DcMotorSimple.Direction.FORWARD);
-        w3.setDirection(DcMotorSimple.Direction.FORWARD);
+        RobotMotorsSetup.init(hardwareMap);
 
         waitForStart();
 
