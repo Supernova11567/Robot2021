38ad045a610763e09ebfc5fe930f4c2826e49257
Idan <idanro12@gmail.com>
2020-11-08 10:29:00 +0200

    Update Supernova11567Autonomous.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Supernova11567Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Supernova11567Autonomous.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.teamcode.RobotMotorsSetup;
 
 @Autonomous(name = "Supernova11567Autonomous", group = "Supernova11567")
@@ -15,6 +16,7 @@ public class Supernova11567Autonomous extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
 
     private org.firstinspires.ftc.teamcode.RobotMotorsSetup RobotMotorsSetup = new RobotMotorsSetup(hardwareMap, gamepad1, gamepad2);
+    private Supernova11567Vuforia Supernova11567Vufofria = new Supernova11567Vuforia(VuforiaLocalizer.CameraDirection.BACK, VuforiaLocalizer.Parameters.CameraMonitorFeedback.AXES, );
     /* motors configuration on robot:
                                            /|\ forward
                                          1     0
