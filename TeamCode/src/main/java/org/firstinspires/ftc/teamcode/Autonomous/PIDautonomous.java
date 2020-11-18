46f17648efee133ec96fea8cde048a9febe9fa61
Idan <idanro12@gmail.com>
2020-11-18 15:48:37 +0200

    Update PIDautonomous.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PIDautonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/PIDautonomous.java
@@ -6,11 +6,22 @@ public class PIDautonomous {
 
     PIDCoefficients pidCoefficients;
 
+    double startPosition = 0;
+    double startTime = 0;
+
 
     /* constructor */
     public PIDautonomous (double kp, double ki, double kd) {
         pidCoefficients = new PIDCoefficients(kp, ki, kd);
     }
-    
 
+    public void resetAllCalculations () {
+
+    }
+
+    public double PID_start (double startPosition, double starTime) {
+        
+        this.startPosition = startPosition;
+        this.startTime = starTime;
+    }
 }
