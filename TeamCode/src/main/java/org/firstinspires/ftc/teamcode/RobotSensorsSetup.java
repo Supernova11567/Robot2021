fe4a491c091df9448b89753bbfac98d59c571717
Idan <idanro12@gmail.com>
2020-11-18 13:29:28 +0200

    Create RobotSensorsSetup.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotSensorsSetup.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class RobotSensorsSetup {
+    public DistanceSensor distanceSensor = null;
+
+
+    /* local OpMode members. */
+    com.qualcomm.robotcore.hardware.HardwareMap HardwareMap = null;
+    private Gamepad gamepad1 = null;
+    private Gamepad gamepad2 = null;
+    private ElapsedTime period = new ElapsedTime();
+
+    /* Constructor */
+    public RobotSensorsSetup(HardwareMap hwMap, Gamepad gamepad1, Gamepad gamepad2) {
+        HardwareMap = hwMap;
+        this.gamepad1 = gamepad1;
+        this.gamepad2 = gamepad2;
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init() {
+        // Define and Initialize Motors
+
+    }
+
+    public void loop() {
+
+    }
+}
